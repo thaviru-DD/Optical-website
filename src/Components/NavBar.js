afb569f43cc227fae16bd57d29f26c213f72e544
Thaviru <tsathmira@gmail.com>
2026-02-22 20:12:27 +0530

    Style navigation bar and add cover image

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,31 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavBar.css'
 
 export default function NavBar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div class="container abel-regular">
-        {/* navigation bar */}
-      <nav class="navbar">
+    <div className="container abel-regular">
+
+      <nav className="navbar">
+
         <h2>R.A.Jayasinghe Optical</h2>
-        <ul class='nav-links'>
+
+        <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
             <li><a href="#">Home</a></li>
             <li><a href="#">Contact</a></li>
             <li><a href="#">Location</a></li>
             <li><a href="#">About-us</a></li>
+            <button className="signin-btn mobile-btn">Sign-in</button>
         </ul>
-        <button class="signin-btn">Sign-in</button>
+
+        <button className="signin-btn desktop-btn">Sign-in</button>
+
+        <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </div>
+
       </nav>
 
     </div>
   )
 }
-
-
-
-
-
-
-
-
-
-
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
+import './Home.css'
 
 export default function Home() {
   return (
     <div>
+      {/* add navbar component */}
       <NavBar/>
+
+      {/* hero section */}
+      <div className="hero">
+
+        <img src="/images/expressive-young-woman-posing.jpg" className='hero-img' alt=""/>
+
+        <div className="hero-container">
+          <h1 className='hero-title'>Discover Your Style</h1>
+          <div className="buttons">
+            <button className='shopNow-btn'>Shop Now</button>
+            <button className='contact-btn'>Contact</button>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
